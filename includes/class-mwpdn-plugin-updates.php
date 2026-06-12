42ba491ce7da3be2a085278b282ba6af53a20f5a
Jos Klever <info@joskleverwebsupport.nl>
2026-06-13 00:28:47 +0200

    Fix activation hook for scheduled update-check cron (#20)

--- includes/class-mwpdn-plugin-updates.php
+++ includes/class-mwpdn-plugin-updates.php
@@ -60,7 +60,7 @@ class Plugin_Updates {
 	 * Setup hooks and filters.
 	 */
 	private function setup_hooks() {
-		add_action( 'mainwp_child_plugin_activated', array( $this, 'setup_plugin_update_hook' ) );
+		register_activation_hook( MAINWP_DISCORD_DIR . 'mainwp-discord-notifications.php', array( $this, 'setup_plugin_update_hook' ) );
 		add_action( 'mainwp_cronupdatescheck_action', array( $this, 'check_for_plugin_updates' ) );
 		// Ensure our scheduled hook runs the checker.
 		add_action( 'sprucely_mwpdn_check_for_plugin_updates', array( $this, 'check_for_plugin_updates' ) );
